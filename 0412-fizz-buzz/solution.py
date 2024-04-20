6126e4c19e2a94ac2aa37f5e918aa3c9aebdc22b
Sam Gabel <96152742+samgabel@users.noreply.github.com>
2024-04-20 09:28:09 +0000

    [LeetCode Sync] Runtime - 40 ms (75.58%), Memory - 17.7 MB (19.14%)

--- 0412-fizz-buzz/solution.py
+++ 0412-fizz-buzz/solution.py
@@ -2,12 +2,13 @@ class Solution:
     def fizzBuzz(self, n: int) -> list[str]:
         result = []
         for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
-                result.append("FizzBuzz")
-            elif i % 3 == 0:
-                result.append("Fizz")
-            elif i % 5 == 0:
-                result.append("Buzz")
-            else:
-                result.append(f"{i}")
+            current_string = ""
+            if i % 3 == 0:
+                current_string += "Fizz"
+            if i % 5 == 0:
+                current_string += "Buzz"
+            if i % 3 != 0 and i % 5 != 0:
+                current_string = f"{i}"
+            result.append(current_string)
         return result
+
