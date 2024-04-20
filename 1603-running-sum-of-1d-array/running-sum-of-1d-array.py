5bec3bf742e870cd4e2223b2edc657ae35538434
Sam Gabel <sam@gabelcloud.com>
2024-04-20 00:58:18 -0700

    Delete previous LeetCode submissions
    using LeetCode Sync GitHub Action instead of LeetCode Sync Chrome
    extension
    
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        # single pass inplace
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-        return nums
